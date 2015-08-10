3d8a3b99f458d42c95856697a872447c3dc9faff
niuqinghua <niuqinghua@letv.com>
2015-08-10 16:43:09 +0800

    add socket sample using gzip object transmit

---
+++ src/main/java/net/learning/sample/socket/sample4/User.java
@@ -0,0 +1,39 @@
+package net.learning.sample.socket.sample4;
+
+import java.io.Serializable;
+
+/**
+ * Created by niuqinghua on 15/8/10.
+ */
+public class User implements Serializable {
+
+    private String name;
+
+    private String password;
+
+    public User() {
+
+    }
+
+    public User(String name, String password) {
+        this.name = name;
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}
